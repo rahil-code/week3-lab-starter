7a19479396238c28bf2d9a1c84ef33a7432b362a
rahil-code <mok865123@gmail.com>
2026-01-20 11:32:51 -0700

    feat(courses): complete CourseId type and Course interface

--- src/types/course.ts
+++ src/types/course.ts
@@ -15,4 +15,17 @@
 // once all group members have completed their sections, add the following field:
 // an optional array of program ids that this course is part of
 
-export interface Course {}
+export interface Course {
+  id: CourseID;
+  code: string;
+  title: string;
+  description: string;
+  
+  credit: number;
+  term: number;
+  delivery: Delivery;
+  preRequisites: CourseID[];
+  note: string;
+  programIds: string[];
+}export type CourseID = string;
+import { Delivery } from "./common";
